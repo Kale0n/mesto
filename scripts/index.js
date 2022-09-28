ea530065f8ccb3d3c55549901c29c38687c73d6a
Kaleon <Kaleon612@gmail.com>
2022-09-29 00:35:42 +0300

    ПР5. 1 итерпция

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,140 @@
-const popup = document.querySelector('.popup'); //ищем в DOMe сам попап
+//переменные для попапов
+const editPopup = document.querySelector('.popup_button_edit'); //ищем в DOMe сам попап
 const editButton = document.querySelector('.profile__edit-button'); //ищем кнопку "редактировать"
-const closeButton = document.querySelector('.popup__close-button'); //ищем кнопку "закрыть"
-const form = document.forms.editForm; //ищем форму по имени "name"
-const nameInput = form.elements.name; //ищем инпут с именем "occupation"
-const occupationInput = form.elements.occupation; //ищем инпут с работой
-const profileName = document.querySelector('.profile__name'); // создает еще одно переменную, которой находим непосредственно имя, укащанное в профиле. 
-const profileOccupation = document.querySelector('.profile__occupation');
+const addPopup = document.querySelector('.popup_button_add');
+const addButton = document.querySelector('.profile__add-button');
+const closeButtons = Array.from(document.querySelectorAll('.popup__close-button')); //ищем все кнопки "закрыть". Из списка узлов делаем массив
+const zoomPopup= document.querySelector('.popup_zoom');
 
-function editProfile() { //функция, которая добавляет класс к попапу и, тем самым, делает попап видимым. Фокусы на грани магии. 
-    popup.classList.add('popup_opened'); 
-    nameInput.value = profileName.textContent;
-    occupationInput.value = profileOccupation.textContent;
+//переменные для формы редактирования
+const editForm = document.forms.editForm; 
+const nameInput = editForm.elements.name; //ищем форму по имени "name"
+const occupationInput = editForm.elements.occupation; //ищем инпут с работой
+const profileName = document.querySelector('.profile__name'); // переменная, в которой находится имя, укащанное в профиле. 
+const profileOccupation = document.querySelector('.profile__occupation'); // по похожей логике создаем переменную с родом занятий
+
+//перемннные для формы создания новой карточки 
+const addCardForm = document.forms.addForm; 
+const placeInput = addCardForm.elements.newPlace;
+const linkInput = addCardForm.elements.link;
+
+//открытие всех карточек. 
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function closePopup() { //функция, которая убирает класс из попапа. По умолчанию у всех кнопок тип "submit", поэтому нажатие на кнопку автоматически отправляет форму. Если прописать кнопке тип кнопки, то она станет просто кнопкой, и никаких других функций не потребуется. 
-    popup.classList.remove('popup_opened');
+// Закрытие всех карточек попапа. 
+function closePopup(popup) {
+  popup.classList.remove('popup_opened')
+}
+
+function closePopupFromEvent(event) { 
+  closePopup(event.target.closest('.popup_opened'))
+} 
+
+closeButtons.forEach((element) => {element.addEventListener('click', closePopupFromEvent)}) // перебираем все кнопки в массиве и на каждую вешаем слушатель, который должен будет закрыть окно 
+
+
+// открыть попап с редактированием профиля
+function editProfile() { //функция, которая добавляет класс к попапу и, тем самым, делает попап видимым. Фокусы на грани магии. 
+  openPopup(editPopup) 
+  nameInput.value = profileName.textContent;
+  occupationInput.value = profileOccupation.textContent;
 }
 
 editButton.addEventListener('click', editProfile); //добавляем кнопке "редактировать" слушатель, который по клику на кнопку вызовет функцию
-closeButton.addEventListener('click', closePopup); //добавляем кнопке "закрыть" слушатель, который по клику на кнопку вызовет функцию
 
+// Редактирование информации в профиле. 
 function formSubmitHandler (evt) { //функция сохраняет данные и изменяет имя и занятие пользователся
-    evt.preventDefault(evt); // предотвращение стандартного события 
-    profileName.textContent = nameInput.value; ; //меняем текст второй переменной на значение, полученное первой переменной. 
-    profileOccupation.textContent = occupationInput.value; 
+  evt.preventDefault(evt); // предотвращение стандартного события 
+  profileName.textContent = nameInput.value; ; //меняем текст второй переменной на значение, полученное первой переменной. 
+  profileOccupation.textContent = occupationInput.value; 
 
-    closePopup(); //вызываем функцию закрытия попапа. 
+  closePopupFromEvent(evt); //вызываем функцию закрытия попапа. 
 }
 
-form.addEventListener('submit', formSubmitHandler); //эвентлисенер на форму. Лисенер слушаент, не отправлена ли форма, и запускает функцию. 
+editForm.addEventListener('submit', formSubmitHandler); //эвентлисенер на форму. Лисенер слушаент, не отправлена ли форма, и запускает функцию. 
+
+
+// открыть попап с добавлением карточки
+addButton.addEventListener('click', (evt) => {openPopup(addPopup)})
+
+
+
+//работа с шаблоном для карточек. 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+const cardTemplate = document.querySelector('#card').content; //ищем шаблон в html
+const cardsContainer = document.querySelector('.elements')
+
+
+function addCard(card, i) { //общая функция добавления карточек 
+  card.querySelector('.element__title').textContent = initialCards[i].name;
+  card.querySelector('.element__photo').src = initialCards[i].link;
+  card.querySelector('.element__photo').alt = initialCards[i].name;
+  card.querySelector('.element__like-button').addEventListener('click', (event) => { event.target.classList.toggle('element__like-button_active'); }); //кнопка лайка
+  card.querySelector('.element__delete-button').addEventListener('click', (event) => { event.target.closest('.element').remove(); }); //кнопка удаления карточки 
+  card.querySelector('.element__photo-button').addEventListener('click', openPhotoPopup) //открытие большой картинки нажатием. 
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  const card = cardTemplate.querySelector('.element').cloneNode(true);
+  addCard(card, i);
+  cardsContainer.append(card)
+}
+
+// открытие картинки отдельным попапом
+
+function openPhotoPopup (event){
+  openPopup(zoomPopup)
+  const photo = document.querySelector('.zoom__photo');
+  photo.src = event.target.src;
+  photo.alt = event.target.alt;
+
+  const photoCaption = document.querySelector('.zoom__caption');
+  photoCaption.textContent = event.target.alt;
+}
+
+// Добавление новой карточки через кнопку. 
+
+function addFormSubmit (evt) {
+  evt.preventDefault(evt);
+  initialCards.unshift({name:placeInput.value, link:linkInput.value})
+
+  const newCard = cardTemplate.querySelector('.element').cloneNode(true)
+  addCard(newCard, 0)
+  
+  cardsContainer.prepend(newCard)
+
+  placeInput.value = '';
+  linkInput.value = '';
+
+  closePopupFromEvent(evt)
+ } 
+
+addCardForm.addEventListener('submit', addFormSubmit)
